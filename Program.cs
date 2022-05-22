1d8c59807a18735d49567ad5fc6c2ce3f910d3d4
ClintonSBarnes <clintbarnes8@gmail.com>
2022-05-22 16:08:17 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string input;
+int tickOrTock;
+Console.Write("What is the clock tower input number? ");
+
+input = Console.ReadLine();
+tickOrTock = Convert.ToInt32(input);
+
+if (tickOrTock % 2 == 0)
+{
+    Console.WriteLine("Tick");
+}
+Console.WriteLine("Tock");
+
+
+Console.ReadKey();
